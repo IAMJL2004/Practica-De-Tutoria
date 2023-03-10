9b048f922053124e84f2b0f9f12c65a7dc7c13a7
IAMJL2004 <126819372+IAMJL2004@users.noreply.github.com>
2023-03-10 12:23:04 -0600

    Create practica04-03-23.py

---
+++ practica04-03-23.py
@@ -0,0 +1,27 @@
+#Operadores logicos
+
+n1=3
+n2=6
+
+resultado=n1<n2
+print(resultado)
+
+#Operadpres aritmeticos " +, - , /, //, %, ** "
+
+num1=20
+num2=8
+
+#otra forma de hacer el cod: num1+=num2
+#print(num1)
+
+resul=num1+num2
+print(f"El resultado de sumar {num1} + {num2} = {resul}") #f para concatenar
+
+#el modulo sirve para encontrar el reciduo de dos valores
+# la division entera sirve para dar un numero entero, la mision es eliminar el decimal
+
+res=num1**num2
+print(f"El resultado de la elevacion del numero {num1} elevado al {num2} = {res}")
+
+R=num1%num2
+print(R)
